fbd7314860bbce0cb569ddf52dedce6a701ba23a
Markus Simonsen <markus.simonsen@gmail.com>
2026-01-25 11:03:56 +0100

    Updated test interface

--- public/script.js
+++ public/script.js
@@ -1,13 +1,27 @@
 // Fetch http://localhost:3000/api?lat=-33.4489&lon=-70.6693
 
+let responseCache = null;
+
 async function main() {
-  let response = await fetch(
+  const response = await fetch(
     "http://localhost:3000/api?lat=-33.4489&lon=-70.6693",
   );
-  let data = await response.json();
-  console.log(data);
+  const data = await response.json();
 
-  document.getElementById("output").textContent = JSON.stringify(data, null, 2);
+  const serialized = JSON.stringify(data);
+
+  if (serialized !== responseCache) {
+    document.getElementById("output").textContent = JSON.stringify(
+      data,
+      null,
+      2,
+    );
+    responseCache = serialized;
+  } else {
+    console.log("Same");
+  }
+
+  setTimeout(main, 100);
 }
 
 main();
